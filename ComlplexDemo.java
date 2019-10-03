7f8ff87893b8c8735a4ccf6380e31a686efa883b
vinod <vinod.tyagi222@gmail.com>
2019-10-03 19:52:52 +0530

    added java ComplexDemo java program file

---
+++ ComlplexDemo.java
@@ -0,0 +1,14 @@
+class ComplexDemo
+{
+   public static void main(String args[])
+   {
+        int r1=3,i1=4,r2=5,i2=6,r,i;
+        r=r1+r2;
+        i=i1+i2;
+        System.out.println(r + "+" + i + "i");
+
+   }
+
+
+
+}
