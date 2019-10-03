37c5de27e6ac48aae98c76e2a558f737f360ffbf
vinod <vinod.tyagi222@gmail.com>
2019-10-03 19:42:51 +0530

    commit fot add Breakcont file

---
+++ BreakCont.java
@@ -0,0 +1,16 @@
+class BreakCont
+{
+   public static void main(String args[])
+   {
+     int i=1;
+     boolean flag=true;
+     for(;flag;){
+     if(i==5)
+     flag=false;
+     System.out.println(i);
+     i++;
+}
+
+}
+
+}
